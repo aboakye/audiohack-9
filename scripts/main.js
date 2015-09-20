5b3f31bad605a20da2ed78d2701ea347def44aab
Adwoa Boakye <adwoa@movementdash.com>
2015-09-20 11:44:15 -0400

    playing with creating presentation mode

--- scripts/main.js
+++ scripts/main.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
 	// 	$('.big-quote').html($quoteInput);
 	// });
 	// $audio1.play();
+	//javascript: 
+
+	//CRAZY opens a new window with minimal header || not working with video
+	// window.open(location.href, "detab", "toolbar=0"); 
+	// window.close();
+	// console.log("Runn");
+	
+	// function(e)  {
+ //        var currentAttrValue = jQuery(this).attr('href');
+
+ //        // Show/Hide Tabs
+ //        jQuery('.tabs ' + currentAttrValue).siblings().slideUp(400);
+ //    }
 });
