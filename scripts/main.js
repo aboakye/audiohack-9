63575fdd5a1ce25a927f97d56651071316b57c4d
Annie Daniel <adaniel@texastribune.org>
2015-09-20 11:43:56 -0400

    audio buttons and toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,23 @@
 
-var $quoteInput = $('#quote-input').value;
-
 
 $(document).ready(function() {
-	// var $audio1 = $('audio')[0];
-	// $('.react-btn').click(function(){
-	// 	console.log($quoteInput);
-	// 	$('.big-quote').html($quoteInput);
-	// });
-	// $audio1.play();
+	var serialAud = $('#serial'),
+		serialBtn = $('#serial-btn');
+
+	serialBtn.click(function() {
+		serialAud.get(0).play();
+	});
+
+	var yonceAud = $('#yonce'),
+		yonceBtn = $('#yonce-btn');
+
+	yonceBtn.click(function() {
+		yonceAud.get(0).play();
+	});
+	
+
+	$( "#earmoji-show" ).click(function() {
+		$('.earmoji-plugin-container').toggle('hide');
+	});
+
 });
