a53fbe020bb2b12fd4bd46911688637910572b9f
Annie Daniel <adaniel@texastribune.org>
2015-09-20 12:56:48 -0400

    styling plugin

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
-
-
 $(document).ready(function() {
+	var capturedAudio = $('#captured')[0],
+		playBtn = $('play-btn'),
+		pauseBtn = $('pause-btn'),
+		progress = $('.progress-filled');
+
+	function updateProgress() { //progress bar tracks audio duration and adjusts width according to current time
+	  var value = 0;
+	  if (capturedAudio.currentTime > 0) {
+	     value = Math.floor((100 / capturedAudio.duration) * capturedAudio.currentTime);
+	  }
+	  progress.css('width', value + '%');
+	}
+
+	function playPause() { //on click play and pause buttons toggle
+	  pauseBtn.removeClass('hide');
+	  if (capturedAudio.paused === false) {
+	      capturedAudio.pause();
+	      playBtn.removeClass('hide');
+	      pauseBtn.addClass('hide');
+	  } else {
+	      capturedAudio.play();
+	      playBtn.addClass('hide');
+	      pauseBtn.removeClass('hide');
+	  }
+	}
+
+	playBtn.click(playPause());
+	capturedAudio.addEventListener('timeupdate', updateProgress, false);
+
 	var serialAud = $('#serial'),
 		serialBtn = $('#serial-btn');
 
@@ -14,6 +41,50 @@ $(document).ready(function() {
 	yonceBtn.click(function() {
 		yonceAud.get(0).play();
 	});
+
+	var airhornAud = $('#airhorn'),
+		airhornBtn = $('#airhorn-btn');
+
+	airhornBtn.click(function() {
+		airhornAud.get(0).play();
+	});
+	
+	var gabrielAud = $('#gabriel'),
+		gabrielBtn = $('#gabriel-btn');
+
+	gabrielBtn.click(function() {
+		gabrielAud.get(0).play();
+	});
+
+	var snoopAud = $('#snoop'),
+		snoopBtn = $('#snoop-btn');
+
+	snoopBtn.click(function() {
+
+		snoopAud.get(0).play();
+	});
+
+	var clapAud = $('#clap'),
+		clapBtn = $('#clap-btn');
+
+	clapBtn.click(function() {
+		clapAud.get(0).play();
+	});
+
+	var laughAud = $('#laugh'),
+		laughBtn = $('#laugh-btn');
+
+	laughBtn.click(function() {
+		laughAud.get(0).play();
+	});
+	
+	var sadAud = $('#sad'),
+		sadBtn = $('#sad-btn');
+
+	sadBtn.click(function() {
+		sadAud.get(0).play();
+	});
+	
 	
 
 	$( "#earmoji-show" ).click(function() {
